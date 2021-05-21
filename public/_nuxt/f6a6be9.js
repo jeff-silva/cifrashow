fef0eb038c71ef5834e6e4b78039b05768c3b829
Jeferson Inácio <jeferson.i.silva@gmail.com>
2021-05-21 04:22:09 -0300

    build

---
+++ public/_nuxt/f6a6be9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[48],{689:function(n,e,t){"use strict";t.r(e);var l={middleware:"auth",layout:"admin"},r=t(24),component=Object(r.a)(l,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\r\n    admin\r\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
