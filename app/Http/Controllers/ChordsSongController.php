b00e17bb886af6044477f5b2a0c89d3d31d828ee
Jeferson Inácio <jeferson.i.silva@gmail.com>
2021-05-22 01:16:26 -0300

    song page

--- app/Http/Controllers/ChordsSongController.php
+++ app/Http/Controllers/ChordsSongController.php
@@ -6,13 +6,15 @@ class ChordsSongController extends Controller
 {
 
 	public function getSearch() {
-		return \App\Models\ChordsSong::select(['id', 'user_id', 'artist_id', 'name', 'created_at', 'updated_at'])
+		return \App\Models\ChordsSong::select(['id', 'user_id', 'artist_id', 'slug', 'name', 'created_at', 'updated_at'])
 			->with(['user', 'chordsArtist'])
 			->querySearch();
 	}
 
-	public function getFind($id) {
-		return \App\Models\ChordsSong::find($id);
+	public function getFind($idSlug) {
+		return \App\Models\ChordsSong::with([
+			'chordsArtist',
+		])->findIdSlug($idSlug);
 	}
 
 	public function postSave() {
---
+++ resources/nuxt/pages/chord/_slug.vue
@@ -0,0 +1,32 @@
+<template><div>
+    
+    <!-- Música -->
+    <div v-if="model">
+
+        <!-- Cover -->
+        <div v-if="model.chords_artist && model.chords_artist.cover && model.chords_artist.cover.url">
+            <img :src="model.chords_artist.cover.url" alt="" width="100%" style="width:100%; max-height:300px; object-fit:cover;">
+        </div>
+
+        <div class="container">
+            <h1>{{ model.name }}</h1>
+            <h2 v-if="model.chords_artist">{{ model.chords_artist.name }}</h2>
+        </div>
+
+        <pre>{{ model }}</pre>
+    </div>
+</div></template>
+
+<script>
+export default {
+    data() {
+        return {
+            model: false,
+        };
+    },
+
+    async fetch() {
+        this.model = await this.$axios.$get(`/api/chords-song/find/${this.$route.params.slug}`);
+    },
+}
+</script>
